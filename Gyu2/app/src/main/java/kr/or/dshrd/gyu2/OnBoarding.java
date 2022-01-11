4045e75b228e0f25b2d5d0c42a0a62bebd597a48
dlxorb96 <xorb_1@naver.com>
2022-01-11 17:50:45 +0900

    20220111

---
+++ Gyu2/app/src/main/java/kr/or/dshrd/gyu2/OnBoarding.java
@@ -0,0 +1,38 @@
+package kr.or.dshrd.gyu2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class OnBoarding extends AppCompatActivity {
+
+    Animation topanim, bottomanim;
+    ImageView image;
+    TextView ttit, ttit2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+
+        //변수에 애니메이션 설정하기
+        topanim= AnimationUtils.loadAnimation(this,R.anim.top_animateion);
+        bottomanim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image=findViewById(R.id.board_logo);
+        ttit=findViewById(R.id.board_title);
+        ttit2=findViewById(R.id.board_start);
+
+
+        // 이미지와 텍스트에 애니메이션 연결
+        image.setAnimation(topanim);
+        ttit.setAnimation(topanim);
+        ttit2.setAnimation(bottomanim);
+    }
+}
